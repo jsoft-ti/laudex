5375c6dff9bb71071511512c8b0b2a98943eece1
João Carlos Silva de Souza <jocasiso@gmail.com>
2021-02-20 17:46:19 -0300

    Layout Add Laudo

---
+++ app/src/main/java/br/com/laudex/ui/laudos/AddLaudoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.laudex.ui.laudos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.laudex.R
+
+class AddLaudoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_laudo)
+    }
+}
